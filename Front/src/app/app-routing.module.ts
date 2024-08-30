6b6cf8595b3a2cb99ed3776139a6240e127baa3e
Felipe De Andrade <felipe.deam98@gmail.com>
2024-08-30 18:42:41 -0300

    fix: Connection with repository

---
+++ Front/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { LoginComponent } from './components/login/login.component';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
+import { ClientesComponent } from './components/clientes/clientes.component';
+import { CobrancaComponent } from './components/cobranca/cobranca.component';
+
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'cadastro', component: CadastroComponent},
+  {path: 'clientes', component: ClientesComponent},
+  {path: 'cobranca', component: CobrancaComponent},
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: '**', redirectTo: 'login', pathMatch: 'full'},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
